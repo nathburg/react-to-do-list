a04e6ad7251969ee9acdf37e0a2461c9f62ec2e4
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-29 15:16:43 -0700

    connecting properly to supabase

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,16 +1,33 @@
+import { useState } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import { authUser } from '../../services/auth';
 import './Auth.css';
 
 export default function Auth() {
+  const { authType } = useParams();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return (
     <div>
+      <div className='nav-links'>
+        <NavLink to='/auth/sign-in'>Sign In</NavLink>
+        <NavLink to='/auth/sign-up'>Sign Up</NavLink> 
+      </div>
       <div className='input-container'>
         <label>Email
-          <input />
+          <input onChange={e => setEmail(e.target.value)} />
         </label>
         <label>Password
-          <input />
+          <input onChange={e => setPassword(e.target.value)} />
         </label>
-        <button>Enter</button>
+        <button onClick={async () => {
+          const resp = await authUser(email, password, authType);
+          console.log(resp);
+          console.log(authType);
+        }}>Enter
+        </button>
       </div>
     </div>
   );
