5c05b56527a171a6697dca9766ff1c7a57c1716a
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-29 22:31:01 -0700

    users can check off items and delete them

--- src/Components/ToDoItem/ToDoItem.js
+++ src/Components/ToDoItem/ToDoItem.js
@@ -1,14 +1,24 @@
 import './ToDoItem.css';
 import { deleteToDoItem } from '../../services/client';
+import { useState } from 'react';
 
 export default function ToDoItem({ description, id, setSubmit, submit }) {
+  const [taskComplete, setTaskComplete] = useState(false);
   return (
     <div className='to-do-item'>
       <div>{description}</div>
-      <div className='checkbox' onClick={async () => {
+      {!taskComplete &&
+      <div className='checkbox' onClick={() => setTaskComplete(true)}>⬜
+      </div>} 
+      {taskComplete &&
+      <div className='checkbox'>✅
+      </div>}
+      <div className='delete' onClick={async () => {
         await deleteToDoItem(id);
         setSubmit(!submit);
-      }}>✅</div>
-    </div>
+      }}>❌
+      </div>
+    </div>  
   );
 }
+
